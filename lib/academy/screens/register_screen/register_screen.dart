a41cb36c03de1cc7fdd59784d002db2c111b3d21
Dmitriy Kudasov <dev.kudasov@gmail.com>
2025-02-18 22:40:22 +0300

    Translations for signin/signup

--- lib/academy/screens/register_screen/register_screen.dart
+++ lib/academy/screens/register_screen/register_screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:teachblox/widgets/main_app_bar.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
-      child: Text('RegisterScreen'),
+    return Scaffold(
+      appBar: MainAppBar(),
     );
   }
 }
