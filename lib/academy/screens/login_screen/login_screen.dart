d06dc4cf197c8123434e9f5ca9ef00244750dd71
Kudasov Dmitriy <dev.kudasov@gmail.com>
2025-02-26 16:26:50 +0300

    Login screen ui (#13)

--- lib/academy/screens/login_screen/login_screen.dart
+++ lib/academy/screens/login_screen/login_screen.dart
@@ -1,12 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:teachblox/academy/routing/routes.dart';
+import 'package:teachblox/l10n/locals.dart';
+import 'package:teachblox/utils.dart';
+import 'package:teachblox/widgets/buttons/button.dart';
+import 'package:teachblox/widgets/input.dart';
+import 'package:teachblox/widgets/layout/adaptive_padding.dart';
+import 'package:teachblox/widgets/main_app_bar.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
-      child: Text('LoginScreen'),
+    final locale = getLocale(context);
+
+    return Scaffold(
+      appBar: MainAppBar(),
+      body: AdaptivePadding(
+        child: Center(
+          child: SizedBox(
+            width: getAdaptiveValue(
+              context,
+              mobile: double.infinity,
+              tablet: double.infinity,
+              desktop: 400.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      locale.loginScreen_do_not_have_account_link_text,
+                      style: getLabelMedium(context),
+                    ),
+                    SizedBox(width: 12.0),
+                    Button(
+                      variant: ButtonVariant.link,
+                      onPressed: () => context.replace(registerScreenRoute),
+                      text: locale.sign_up,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 36.0),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    '${locale.email}*',
+                    style: getLabelSmall(context),
+                  ),
+                ),
+                SizedBox(height: 12.0),
+                Input(),
+                SizedBox(height: 24.0),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    '${locale.password}*',
+                    style: getLabelSmall(context),
+                  ),
+                ),
+                SizedBox(height: 12.0),
+                Input(isPassword: true),
+                SizedBox(height: 32.0),
+                Button(
+                  fullWidth: true,
+                  onPressed: () {},
+                  text: locale.log_in,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
